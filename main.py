817920a75487b36626cc80c4e59e41462f93227b
Jose Carlos <jocdiazmuic@gmail.com>
2024-08-27 17:39:59 -0500

    adding player and rendering triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
@@ -9,12 +10,20 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
